bb8d0290a6601203abbf8e60a7f0136a6b980546
d0c0q <d0c0q@otterclam.finance>
2021-12-17 11:10:32 +0800

    feat: pearl

---
+++ scripts/deployPearl.js
@@ -0,0 +1,17 @@
+const { ethers } = require('hardhat')
+
+async function main() {
+  const StakedCLAM = await ethers.getContractFactory('StakedOtterClamERC20V2')
+  const sCLAM = StakedCLAM.attach('0xAAc144Dc08cE39Ed92182dd85ded60E5000C9e67')
+
+  const PEARL = await ethers.getContractFactory('OtterPearlERC20')
+  const pearl = await PEARL.deploy(sCLAM.address)
+  console.log(`done, pearl address: ${pearl.address}`)
+}
+
+main()
+  .then(() => process.exit())
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
