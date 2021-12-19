cf9c52b0f1078a4f393625c510ba63e52e8ce0e8
d0c0q <d0c0q@otterclam.finance>
2021-12-19 18:14:48 +0800

    docs: pearl

--- scripts/deployPearl.js
+++ scripts/deployPearl.js
@@ -1,11 +1,8 @@
 const { ethers } = require('hardhat')
 
 async function main() {
-  const StakedCLAM = await ethers.getContractFactory('StakedOtterClamERC20V2')
-  const sCLAM = StakedCLAM.attach('0xAAc144Dc08cE39Ed92182dd85ded60E5000C9e67')
-
   const PEARL = await ethers.getContractFactory('OtterPearlERC20')
-  const pearl = await PEARL.deploy(sCLAM.address)
+  const pearl = await PEARL.deploy('0xAAc144Dc08cE39Ed92182dd85ded60E5000C9e67')
   console.log(`done, pearl address: ${pearl.address}`)
 }
 
